4ac688540b91a5b376a1b2a909eba6e5c833f056
Baptiste Brun-Arasa <brunarasa@gmail.com>
2020-06-29 10:04:48 +0200

    [artemisNG#1523] places and geo_status diff in failures report
    WIP

--- artemis/base_pytest.py
+++ artemis/base_pytest.py
@@ -564,12 +564,30 @@ class ArtemisTestFixture(CommonTestFixture):
             if six.PY3:
                 from artemis import pytest_report_makers
 
-                report_message = pytest_report_makers.journeys_diff(
+                journeys_report_message = pytest_report_makers.journeys_diff(
                     filtered_reference, filtered_response
                 )
-                pytest_report_makers.add_to_report(
-                    self.get_test_name(), http_query, report_message
+
+                places_report_message = pytest_report_makers.places_diff(
+                    filtered_reference, filtered_response
                 )
+
+                geo_status_report_message = pytest_report_makers.geo_status_diff(
+                    filtered_reference, filtered_response
+                )
+
+                pytest_report_makers.add_to_report(
+                    self.get_test_name(), http_query, journeys_report_message
+                )
+
+                pytest_report_makers.add_to_report(
+                    self.get_test_name(), http_query, places_report_message
+                )
+
+                pytest_report_makers.add_to_report(
+                    self.get_test_name(), http_query, geo_status_report_message
+                )
+
             # Print difference in console
             # print_diff(response_filepath, output_reference_filepath, self.get_test_name())
 
--- artemis/pytest_report_makers.py
+++ artemis/pytest_report_makers.py
@@ -1,38 +1,71 @@
 import ujson as json
-import jsondiff
 import os
 from artemis.configuration_manager import config
+from deepdiff import DeepDiff
 import urllib.parse
 
 
 def journeys_diff(ref_dict, resp_dict):
     ref_journeys = ref_dict.get("journeys", [])
     resp_journeys = resp_dict.get("journeys", [])
+    diff = DeepDiff(ref_journeys, resp_journeys)
+    message = (
+        "<ul><li>new journeys nb: {}\n</li>"
+        "<li>discarded journeys nb: {}\n</li>"
+        "<li>updated journeys nb: {}\n</li></ul>"
+        "<details open><summary>CLICK ME</summary><p>\n\n"
+        "<pre><code class='language-json\n'>"
+        "{}\n"
+        "</p></details>\n</code></pre>"
+    ).format(
+        len(diff.get("dictionary_item_added", [])),
+        len(diff.get("dictionary_item_removed", [])),
+        len(diff.get("values_changed", [])),
+        json.dumps(diff, indent=2),
+    )
+    return message
 
-    if not ref_journeys and not resp_journeys:
-        diff = {}
-    elif not ref_journeys and resp_journeys:
-        diff = {jsondiff.symbols.insert: [[i, j] for i, j in enumerate(resp_journeys)]}
-    elif ref_journeys and not resp_journeys:
-        diff = {jsondiff.symbols.delete: [[i, j] for i, j in enumerate(ref_journeys)]}
-    else:
-        diff = jsondiff.diff(ref_journeys, resp_journeys, syntax="symmetric")
 
-    updated_nb = sum(str(k).isdigit() for k in diff.keys())
+def places_diff(ref_dict, resp_dict):
+    ref_places = ref_dict.get("places", [])
+    resp_places = resp_dict.get("places", [])
+    diff = DeepDiff(ref_places, resp_places)
+    message = (
+        "<ul><li>new places nb: {}\n</li>"
+        "<li>discarded places nb: {}\n</li>"
+        "<li>updated places nb: {}\n</li></ul>"
+        "<details open><summary>CLICK ME</summary><p>\n\n"
+        "<pre><code class='language-json\n'>"
+        "{}\n"
+        "</p></details>\n</code></pre>"
+    ).format(
+        len(diff.get("dictionary_item_added", [])),
+        len(diff.get("iterable_item_removed", [])),
+        len(diff.get("values_changed", [])),
+        json.dumps(diff, indent=2),
+    )
+    return message
+
+
+def geo_status_diff(ref_dict, resp_dict):
+    ref_geo_status = ref_dict.get("geo_status", [])
+    resp_geo_status = resp_dict.get("geo_status", [])
+    diff = DeepDiff(ref_geo_status, resp_geo_status)
+    print(ref_geo_status)
+    print(resp_geo_status)
 
     message = (
-        "* new journeys nb: {}\n"
-        "* discarded journeys nb: {}\n"
-        "* updated journeys nb: {}\n"
+        "<ul><li>new geo_status nb: {}\n</li>"
+        "<li>discarded geo_status nb: {}\n</li>"
+        "<li>updated geo_status nb: {}\n</li></ul>"
         "<details open><summary>CLICK ME</summary><p>\n\n"
-        "```json\n"
+        "<pre><code class='language-json\n'>"
         "{}\n"
-        "```\n"
-        "</p></details>\n"
+        "</p></details>\n</code></pre>"
     ).format(
-        len(diff.get(jsondiff.symbols.insert, [])),
-        len(diff.get(jsondiff.symbols.delete, [])),
-        updated_nb,
+        len(diff.get("dictionary_item_added", [])),
+        len(diff.get("dictionary_item_removed", [])),
+        len(diff.get("values_changed", [])),
         json.dumps(diff, indent=2),
     )
     return message
@@ -40,17 +73,17 @@ def journeys_diff(ref_dict, resp_dict):
 
 def add_to_report(test_name, test_query, report_message):
     failures_report_path = os.path.join(
-        config["RESPONSE_FILE_PATH"], "failures_report.md"
+        config["RESPONSE_FILE_PATH"], "failures_report.html"
     )
 
     reading_mode = "a" if os.path.exists(failures_report_path) else "w"
     with open(failures_report_path, reading_mode) as failures_report:
-        failures_report.write("## {}\n".format(test_name))
+        failures_report.write("<p><strong>{}\n</strong></p>".format(test_name))
         encoded = urllib.parse.quote(test_query)
         failures_report.write(
             (
-                "[open query in navitia-playground]"
-                "(http://canaltp.github.io/navitia-playground/play.html?request={})\n"
+                "<p><a href=http://canaltp.github.io/navitia-playground/play.html?request={}\n>"
+                "open query in navitia-playground</a></p>"
             ).format(encoded)
         )
         failures_report.write("{}\n".format(report_message))
